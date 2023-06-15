a63402f008bfab638504b950893b6dd6cf4dc903
root <root@James_stand.localdomain>
2023-06-16 02:50:17 +0800

    update

--- webui_install/docker/webui/frontend/src/config/config.js
+++ webui_install/docker/webui/frontend/src/config/config.js
@@ -20,5 +20,6 @@ if (process.env.NODE_ENV === 'test') {
 // config.API_URL = "http://1.1.1.1:8080"
 // config.API_URL = "http://0.0.0.0:8080"
 config.API_URL = "http://127.0.0.1:8080"
+// config.API_URL = "GGGGGGGGGGGGGGGGGGGGGG"
 // config.API_URL = "http://127.0.0.1:443"
 export default config;
