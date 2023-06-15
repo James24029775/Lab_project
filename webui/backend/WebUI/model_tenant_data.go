ffec56c770bd35910866fb7e2972938ff7fb1447
James24029775 <74500141+James24029775@users.noreply.github.com>
2023-06-15 20:38:43 +0800

    Delete webui directory

--- webui/backend/WebUI/model_tenant_data.go
+++
@@ -1,9 +0,0 @@
-package WebUI
-
-type Tenant struct {
-	MissionId         string `json:"missionId"`
-	MissionName       string `json:"missionName"`
-	// MissionCoordinate string `json:"missionCoordinate"`
-	MYSELF_Longitude string `json:"MYSELF_Longitude"`
-	MYSELF_Latitude string `json:"MYSELF_Latitude"`
-}
